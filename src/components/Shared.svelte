3ea03ca4b615be6130d1d4071bb4d84dd38bb8f7
Nathan <natperson@gmail.com>
2023-07-01 06:53:16 -0700

    move title to html

--- src/components/Shared.svelte
+++ src/components/Shared.svelte
@@ -3,7 +3,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
-    <title>Dog News Comics!</title>
 </svelte:head>
 
 <style>
