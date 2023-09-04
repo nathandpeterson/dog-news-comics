3cc4a4adda16a2ae15fe5bcac6293c697be7eb73
Nathan <natperson@gmail.com>
2023-09-03 18:45:40 -0700

    adds layout component

---
+++ src/routes/page/[slug]/+layout.svelte
@@ -0,0 +1,49 @@
+<div class="outer-wrapper">
+    <div class="image-container">
+        <slot />
+        <a href="/">BACK</a>
+    </div>
+</div>
+
+<style>
+    .outer-wrapper {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        flex-direction: column;
+        align-items: center;
+
+        background-color: #F0FFFF;
+    }
+
+    .image-container {
+        width: 375px;
+    }
+
+    @media (min-width: 600px) {
+        .image-container {
+        width: 600px;
+        }
+    }
+
+    @media (min-width: 900px) {
+        .image-container {
+        width: 800px;
+        }
+    }
+
+    @media (min-width: 900px) {
+        .image-container {
+        width: 800px;
+        }
+    }
+
+
+    a {
+        text-decoration: none;
+        color: #FF1694;
+        font-size: 20px;
+        margin-top: 20px;
+    }
+
+</style>
