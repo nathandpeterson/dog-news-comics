b6775e1118c467e36b40d2444e652fbb20286af6
Nathan <natperson@gmail.com>
2024-05-28 20:07:09 -0700

    adds new page

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,36 +1,50 @@
-export const cardData = [{
-        src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-01/dn-01-cover-400.jpg",
-        href: "/page/1",
-        altText: "Colorful book cover with the words Crack me up Cassie and drawing of black dog",
-        bookNumber: 1,
-        bookTitle: "Crack me up, Cassie!",
-    },
-    {
-        src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-02/dn-02-cover-400.jpg",
-        href: '/page/2',
-        altText: "Colorful book cover with the words Take It Away, Tronya! and drawing of small dog",
-        bookNumber: 2,
-        bookTitle: "Take It Away, Tronya!"
-    },
-    {
-        src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-03/dn-03-cover-400.jpg",
-        href: '/page/3',
-        altText:"Colorful book cover with the words Hooray for Hazel! and drawing of small white dog",
-        bookNumber: 3,
-        bookTitle: "Hooray for Hazel!",
-    },
-    {
-        src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-04/dn-04-cover-400.jpg",
-        href: '/page/4',
-        altText:"Colorful book cover with the words Hello Hank! and drawing of small tan dog",
-        bookNumber: 4,
-        bookTitle: "Hello Hank!",
-    },
-    {
-        src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-05/dn-05-cover-400.JPG",
-        href: '/page/5',
-        altText:"Colorful book cover with the words Salutations Stewert! and drawing of small tan dog",
-        bookNumber: 5,
-        bookTitle: "Salutations Stewert!",
-    },
-]
+export const cardData = [
+  {
+    src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-01/dn-01-cover-400.jpg",
+    href: "/page/1",
+    altText:
+      "Colorful book cover with the words Crack me up Cassie and drawing of black dog",
+    bookNumber: 1,
+    bookTitle: "Crack me up, Cassie!",
+  },
+  {
+    src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-02/dn-02-cover-400.jpg",
+    href: "/page/2",
+    altText:
+      "Colorful book cover with the words Take It Away, Tronya! and drawing of small dog",
+    bookNumber: 2,
+    bookTitle: "Take It Away, Tronya!",
+  },
+  {
+    src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-03/dn-03-cover-400.jpg",
+    href: "/page/3",
+    altText:
+      "Colorful book cover with the words Hooray for Hazel! and drawing of small white dog",
+    bookNumber: 3,
+    bookTitle: "Hooray for Hazel!",
+  },
+  {
+    src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-04/dn-04-cover-400.jpg",
+    href: "/page/4",
+    altText:
+      "Colorful book cover with the words Hello Hank! and drawing of small tan dog",
+    bookNumber: 4,
+    bookTitle: "Hello Hank!",
+  },
+  {
+    src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-05/dn-05-cover-400.JPG",
+    href: "/page/5",
+    altText:
+      "Colorful book cover with the words Salutations Stewert! and drawing of small tan dog",
+    bookNumber: 5,
+    bookTitle: "Salutations Stewert!",
+  },
+  {
+    src: "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev/dn-06/dn-06-cover.JPG",
+    href: "/page/6",
+    altText:
+      "Colorful book cover with the words Make it Move Montana! and drawing of small tan dog",
+    bookNumber: 6,
+    bookTitle: "Make It Move Montana!",
+  },
+];
--- src/routes/page/[slug]/+page.server.js
+++ src/routes/page/[slug]/+page.server.js
@@ -1,81 +1,86 @@
-import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_URL } from '$env/static/public'
-import { PRIVATE_SUPABASE_ANON_KEY } from '$env/static/private';
+import { createClient } from "@supabase/supabase-js";
+import { PUBLIC_SUPABASE_URL } from "$env/static/public";
+import { PRIVATE_SUPABASE_ANON_KEY } from "$env/static/private";
 
-const BASE_URL = 'https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev';
+const BASE_URL = "https://pub-d5040c5e154b42b1b0c92c01d2202bfe.r2.dev";
 
 // /** @type { import('./$types').PageLoad} */
 export async function load({ params: { slug } }) {
-    const allPages = [
-        [], // placeholder allows us to use slug as index
-        [
-            `${BASE_URL}/dn-01/dn-p-01-01.jpg`,
-            `${BASE_URL}/dn-01/dn-p-01-02.jpg`,
-            `${BASE_URL}/dn-01/dn-p-01-03.jpg`
-        ],
-        [
-            `${BASE_URL}/dn-02/dn-p-02-01.jpg`,
-            `${BASE_URL}/dn-02/dn-p-02-02.jpg`,
-            `${BASE_URL}/dn-02/dn-p-02-03.jpg`,
-            `${BASE_URL}/dn-02/dn-p-02-04.jpg`,
-            `${BASE_URL}/dn-02/dn-p-02-05.jpg`,
-        ],
-        [
-            `${BASE_URL}/dn-03/dn-p-03-01.jpg`,
-            `${BASE_URL}/dn-03/dn-p-03-02.jpg`,
-            `${BASE_URL}/dn-03/dn-p-03-03.jpg`,
-            `${BASE_URL}/dn-03/dn-p-03-04.jpg`,
-        ],
-        [
-            `${BASE_URL}/dn-04/dn-p-04-01.jpg`,
-            `${BASE_URL}/dn-04/dn-p-04-02.jpg`,
-            `${BASE_URL}/dn-04/dn-p-04-03.jpg`,
-        ],
-        [
-            `${BASE_URL}/dn-05/dn-p-05-01.JPG`,
-            `${BASE_URL}/dn-05/dn-p-05-02.JPG`,
-        ],
-    ]
+  const allPages = [
+    [], // placeholder allows us to use slug as index
+    [
+      `${BASE_URL}/dn-01/dn-p-01-01.jpg`,
+      `${BASE_URL}/dn-01/dn-p-01-02.jpg`,
+      `${BASE_URL}/dn-01/dn-p-01-03.jpg`,
+    ],
+    [
+      `${BASE_URL}/dn-02/dn-p-02-01.jpg`,
+      `${BASE_URL}/dn-02/dn-p-02-02.jpg`,
+      `${BASE_URL}/dn-02/dn-p-02-03.jpg`,
+      `${BASE_URL}/dn-02/dn-p-02-04.jpg`,
+      `${BASE_URL}/dn-02/dn-p-02-05.jpg`,
+    ],
+    [
+      `${BASE_URL}/dn-03/dn-p-03-01.jpg`,
+      `${BASE_URL}/dn-03/dn-p-03-02.jpg`,
+      `${BASE_URL}/dn-03/dn-p-03-03.jpg`,
+      `${BASE_URL}/dn-03/dn-p-03-04.jpg`,
+    ],
+    [
+      `${BASE_URL}/dn-04/dn-p-04-01.jpg`,
+      `${BASE_URL}/dn-04/dn-p-04-02.jpg`,
+      `${BASE_URL}/dn-04/dn-p-04-03.jpg`,
+    ],
+    [`${BASE_URL}/dn-05/dn-p-05-01.JPG`, `${BASE_URL}/dn-05/dn-p-05-02.JPG`],
+    [`${BASE_URL}/dn-06/dn-p-06-01.JPG`, `${BASE_URL}/dn-06/dn-p-06-02.JPG`],
+  ];
 
-    const issueNumber = Number(slug)
-    const pages = allPages[issueNumber];
+  const issueNumber = Number(slug);
+  const pages = allPages[issueNumber];
 
-    const client = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_ANON_KEY, {
-        auth: { persistSession: false }
-    })
-    const { data } = await client.from('comments')
-        .select('*')
-        .eq('is_approved', 'true')
-        .eq('page_id', slug)
-        .order('created_at', { ascending: false });
-        
-    return {
-        comments: data,
-        pages,
-    }
+  const client = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_ANON_KEY, {
+    auth: { persistSession: false },
+  });
+  const { data } = await client
+    .from("comments")
+    .select("*")
+    .eq("is_approved", "true")
+    .eq("page_id", slug)
+    .order("created_at", { ascending: false });
+
+  return {
+    comments: data,
+    pages,
+  };
 }
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-    default: async ({ request, params }) => {
-        const data = await request.formData();
-        const username = data.get('username');
-        const comment = data.get('comment');
-        const client = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_ANON_KEY, {
-            auth: { persistSession: false }
-        });
+  default: async ({ request, params }) => {
+    const data = await request.formData();
+    const username = data.get("username");
+    const comment = data.get("comment");
+    const client = createClient(
+      PUBLIC_SUPABASE_URL,
+      PRIVATE_SUPABASE_ANON_KEY,
+      {
+        auth: { persistSession: false },
+      }
+    );
 
-        await client.from('comments').insert([{
-            page_id: params.slug,
-            display_name: username,
-            comment_text: comment,
-        }])
+    await client.from("comments").insert([
+      {
+        page_id: params.slug,
+        display_name: username,
+        comment_text: comment,
+      },
+    ]);
 
-        await fetch('https://ntfy.sh/dognews', {
-            method: 'POST',
-            body: `New comment from ${username}`
-        })
+    await fetch("https://ntfy.sh/dognews", {
+      method: "POST",
+      body: `New comment from ${username}`,
+    });
 
-        return { success: true };
-    }
+    return { success: true };
+  },
 };
